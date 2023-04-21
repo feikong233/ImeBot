71fba8a5e1baf63f237350c7a37ce72899a06153
BotiIme <62083829+feikong233@users.noreply.github.com>
2023-04-21 14:15:27 +0800

    4.21-现在函数需要传入地区参数，默认值为arcade，为多地区支持做准备

--- botiime/arcade_processing.py
+++ botiime/arcade_processing.py
@@ -3,7 +3,7 @@ import sqlite3
 
 
 # 通过输入的内容判断是否符合数据库中的机厅缩写并查询一次对应机厅的人数
-def arcade_query_return(input_sx):
+def arcade_query_return(input_sx, area=arcade):
     # 初始化变量
     num = 1
     sx = []
@@ -13,17 +13,17 @@ def arcade_query_return(input_sx):
     cur = jtconn.cursor()
 
     # 获取机厅数量jts
-    cur.execute("SELECT COUNT(id) FROM arcade")
+    cur.execute("SELECT COUNT(id) FROM " + area)
     jts = cur.fetchall()
     # 从数据库提取英文机厅缩写，并生成一个列表
     while num <= jts[0][0]:
-        cur.execute("SELECT name FROM arcade WHERE id=" + str(num))
+        cur.execute("SELECT name FROM " + area + " WHERE id=" + str(num))
         sx.append(cur.fetchall())
         num = num + 1
     num = 1
     # 从数据库提取中文机厅缩写，加在列表里
     while num <= jts[0][0]:
-        cur.execute("SELECT chn FROM arcade WHERE id=" + str(num))
+        cur.execute("SELECT chn FROM " + area + " WHERE id=" + str(num))
         sx.append(cur.fetchall())
         num = num + 1
     # 检测输入的机厅缩写是否与数据库中的机厅缩写匹配
@@ -31,9 +31,9 @@ def arcade_query_return(input_sx):
     for i in sx:
         if str(input_sx[0]) == str(i[0][0]):
             # 操作数据库，提取机厅的全名和人数并且存储在对应的变量里
-            cur.execute("SELECT fullname FROM arcade WHERE name='" + str(input_sx[0]) + "' " + "OR chn='" + str(input_sx[0]) + "' ")
+            cur.execute("SELECT fullname FROM " + area + " WHERE name='" + str(input_sx[0]) + "' " + "OR chn='" + str(input_sx[0]) + "' ")
             fullname = str(cur.fetchall()[0][0])
-            cur.execute("SELECT people FROM arcade WHERE name='" + str(input_sx[0]) + "' " + "OR chn='" + str(input_sx[0]) + "' ")
+            cur.execute("SELECT people FROM " + area + " WHERE name='" + str(input_sx[0]) + "' " + "OR chn='" + str(input_sx[0]) + "' ")
             people = str(cur.fetchall()[0][0])
             # 用一个字符串存储查询结果
             query = fullname + " 现在有 " + people + " 人"
@@ -43,7 +43,7 @@ def arcade_query_return(input_sx):
 
 
 # 从数据库获取全部机厅的信息和人数，对应jtj功能
-def people_query():
+def people_query(area = arcade):
     num = 1
     cx = ""
     # 加载数据库并初始化指针
@@ -51,18 +51,18 @@ def people_query():
     cur = jtconn.cursor()
 
     # 从数据库检索id列的行数量，存为jts（机厅数）
-    cur.execute("SELECT COUNT(id) FROM arcade")
+    cur.execute("SELECT COUNT(id) FROM " + area)
     jts = cur.fetchall()
     # 这里是通过num自增遍历一遍数据库里对应的条目，数据库中id列的数值是从1开始的正序整数
     while num <= int(jts[0][0]):
         # 获取对应的机厅名称
-        cur.execute("SELECT fullname FROM arcade WHERE id=" + str(num))
+        cur.execute("SELECT fullname FROM " + area + " WHERE id=" + str(num))
         jtmc = cur.fetchall()
         # 获取对应的机厅人数
-        cur.execute("SELECT people FROM arcade WHERE id=" + str(num))
+        cur.execute("SELECT people FROM " + area + " WHERE id=" + str(num))
         jtrs = cur.fetchall()
         # 获取对应的机厅缩写
-        cur.execute("SELECT name FROM arcade WHERE id=" + str(num))
+        cur.execute("SELECT name FROM " + area + " WHERE id=" + str(num))
         jtsx = cur.fetchall()
         # 生成包含机厅的名称和信息的字符串，然后进入下一次循环
         cx = cx + str(jtsx[0][0]) + " " + str(jtmc[0][0]) + " 现在有 " + str(jtrs[0][0]) + " 人\n"
@@ -88,7 +88,7 @@ def arcade_query(n):
 
 
 # 用于对人数增减消息进行检测和拆分，然后上传到数据库，对应<机厅缩写>[+-]<人数>功能
-def changes_upload(n):
+def changes_upload(n, area=arcade):
     # 初始化变量
     num = 1
     sx = []
@@ -97,17 +97,17 @@ def changes_upload(n):
     cur = jtconn.cursor()
 
     # 获取机厅数量jts
-    cur.execute("SELECT COUNT(id) FROM arcade")
+    cur.execute("SELECT COUNT(id) FROM " + area)
     jts = cur.fetchall()
     # 从数据库提取机厅缩写
     while num <= jts[0][0]:
-        cur.execute("SELECT name FROM arcade WHERE id=" + str(num))
+        cur.execute("SELECT name FROM " + area + " WHERE id=" + str(num))
         sx.append(cur.fetchall())
         num = num + 1
     num = 1
     # 从数据库提取中文机厅缩写，加在列表里
     while num <= jts[0][0]:
-        cur.execute("SELECT chn FROM arcade WHERE id=" + str(num))
+        cur.execute("SELECT chn FROM " + area + " WHERE id=" + str(num))
         sx.append(cur.fetchall())
         num = num + 1
     # 正则表达式，分别匹配 <任意字母或者汉字>[+-]<任意数字>，如ch+1
@@ -120,14 +120,14 @@ def changes_upload(n):
         for i in sx:
             if str(jtsx[0]) == str(i[0][0]):
                 # 获取对应机厅的人数并存储
-                cur.execute("SELECT people FROM arcade WHERE name='" + str(jtsx[0]) + "'")
+                cur.execute("SELECT people FROM " + area + " WHERE name='" + str(jtsx[0]) + "'")
                 rs_past = int(cur.fetchall()[0][0])
                 # 判断正负
                 if str(zj[0]) == '+':
                     rs_now = rs_past + int(jtrs[0])
                     # 数据为增，操作数据库并返回生产的字符串
                     if 0 <= rs_now <= 50:
-                        cur.execute("UPDATE arcade SET people=" + str(rs_now) + " WHERE name='" + str(jtsx[0]) + "'")
+                        cur.execute("UPDATE " + area + " SET people=" + str(rs_now) + " WHERE name='" + str(jtsx[0]) + "'")
                         jtconn.commit()
                         cur.close()
                         return_msg = arcade_query_return(jtsx)
@@ -141,7 +141,7 @@ def changes_upload(n):
                     rs_now = rs_past - int(jtrs[0])
                     if rs_now >= 0:
                         # 计算后数值合理，操纵数据库并返回生成的字符串
-                        cur.execute("UPDATE arcade SET people=" + str(rs_now) + " WHERE name='" + str(jtsx[0]) + "'")
+                        cur.execute("UPDATE " + area + " SET people=" + str(rs_now) + " WHERE name='" + str(jtsx[0]) + "'")
                         jtconn.commit()
                         cur.close()
                         return_msg = arcade_query_return(jtsx)
@@ -159,14 +159,14 @@ def changes_upload(n):
         for i in sx:
             if str(jtsx[0]) == str(i[0][0]):
                 # 获取对应机厅的人数并存储
-                cur.execute("SELECT people FROM arcade WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
+                cur.execute("SELECT people FROM " + area + " WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
                 rs_past = int(cur.fetchall()[0][0])
                 # 判断正负
                 if str(zj[0]) == '+':
                     rs_now = rs_past + int(jtrs[0])
                     # 数据为增，操作数据库并返回生产的字符串
                     if 0 <= rs_now <= 50:
-                        cur.execute("UPDATE arcade SET people=" + str(rs_now) + " WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
+                        cur.execute("UPDATE " + area + " SET people=" + str(rs_now) + " WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
                         jtconn.commit()
                         cur.close()
                         return_msg = arcade_query_return(jtsx)
@@ -180,7 +180,7 @@ def changes_upload(n):
                     rs_now = rs_past - int(jtrs[0])
                     if rs_now >= 0:
                         # 计算后数值合理，操纵数据库并返回生成的字符串
-                        cur.execute("UPDATE arcade SET people=" + str(rs_now) + " WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
+                        cur.execute("UPDATE " + area + " SET people=" + str(rs_now) + " WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
                         jtconn.commit()
                         cur.close()
                         return_msg = arcade_query_return(jtsx)
@@ -193,7 +193,7 @@ def changes_upload(n):
 
 
 # 用于直接上传实际人数的检测及拆分，并上传到数据库，对应<机厅缩写><人数>功能                   
-def number_upload(n):
+def number_upload(n, area = arcade):
     # 初始化变量
     num = 1
     sx = []
@@ -202,17 +202,17 @@ def number_upload(n):
     cur = jtconn.cursor()
 
     # 获取机厅数量jts
-    cur.execute("SELECT COUNT(id) FROM arcade")
+    cur.execute("SELECT COUNT(id) FROM " + area)
     jts = cur.fetchall()
     # 从数据库提取机厅缩写
     while num <= jts[0][0]:
-        cur.execute("SELECT name FROM arcade WHERE id=" + str(num))
+        cur.execute("SELECT name FROM " + area + " WHERE id=" + str(num))
         sx.append(cur.fetchall())
         num = num + 1
     num = 1
     # 从数据库提取中文机厅缩写，加在列表里
     while num <= jts[0][0]:
-        cur.execute("SELECT chn FROM arcade WHERE id=" + str(num))
+        cur.execute("SELECT chn FROM " + area + " WHERE id=" + str(num))
         sx.append(cur.fetchall())
         num = num + 1
     # 正则表达式，分别匹配 <任意字母或汉字><任意数字>，如ch1
@@ -225,7 +225,7 @@ def number_upload(n):
                 # 判断数据是否合理
                 if 0 <= int(jtrs[0]) <= 50:
                     # 数据合理，执行数据库操作并返回生成的字符串
-                    cur.execute("UPDATE arcade SET people=" + str(int(jtrs[0])) + " WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
+                    cur.execute("UPDATE " + area + " SET people=" + str(int(jtrs[0])) + " WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
                     jtconn.commit()
                     cur.close()
                     return_msg = arcade_query_return(jtsx)
@@ -245,7 +245,7 @@ def number_upload(n):
                 # 判断数据是否合理
                 if 0 <= int(jtrs[0]) <= 50:
                     # 数据合理，执行数据库操作并返回生成的字符串
-                    cur.execute("UPDATE arcade SET people=" + str(int(jtrs[0])) + " WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
+                    cur.execute("UPDATE " + area + " SET people=" + str(int(jtrs[0])) + " WHERE name='" + str(jtsx[0]) + "' " + "OR chn='" + str(jtsx[0]) + "' ")
                     jtconn.commit()
                     cur.close()
                     return_msg = arcade_query_return(jtsx)
@@ -259,13 +259,13 @@ def number_upload(n):
 
 
 # 这个函数用来重置数据库中的people列数据，用于每天定时重置数据库中的机厅人数
-def db_reload():
+def db_reload(area=arcade):
     # 初始化数据库的连接并创建指针
     jtconn = sqlite3.connect("./db/arcade.db")
     cur = jtconn.cursor()
     # 重置数据库中people列的所有数据
     cur.execute(
-        "UPDATE arcade SET people = 0"
+        "UPDATE " + area + " SET people = 0"
     )
     jtconn.commit()
     return True
